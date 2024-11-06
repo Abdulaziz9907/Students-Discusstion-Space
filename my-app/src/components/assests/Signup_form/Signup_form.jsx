8a17cd010db0752586c424ebce5461096dc515e9
Abdulaziz9907 <168546745+Abdulaziz9907@users.noreply.github.com>
2024-11-06 20:37:23 +0300

    Signup complete

--- my-app/src/components/assests/Signup_form/Signup_form.jsx
+++ my-app/src/components/assests/Signup_form/Signup_form.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import "./Signup_form.css"
 
 const Signup_form = () => {
+
+
+ 
+
   return (
 
     <div className='SF_box'>
@@ -17,38 +21,94 @@ const Signup_form = () => {
           <input type="text" required />
         </div>
 
+
+        <div className='SF_names'>
+
+        <div className='SF_input-box'>
+          <label for="SF_fname_box" id='SF_fname_text'>First name*</label>
+          <input id='SF_fname_box' type="text" required />
+        </div>
         
-       
         <div className='SF_input-box'>
-          <label id='SF_fname'>First name*</label>
-          <input type="text" required />
+          <label for="SF_lname_box" id='SF_lname_text'>Last name*</label>
+          <input id='SF_lname_box' type="text" required />
         </div>
 
-        <div className='SF_input-box'>
-          <label id='SF_lname'>Last name*</label>
-          <input type="text" required />
         </div>
 
-        <div className='SF_input-box'>
-          <label id='SF_major'>Major*</label>
-          <select name="major" required>
-  <option value="coe">COE</option>
-  <option value="cs">CS</option>
-  
-</select>
+<div className='SF_lists_selectors'>
+
+        <div className='SF_input-list'>
+          <label id='SF_major_text'>Major*</label>
+
+          <datalist id="SF_major" name="major"  required>
+          <option value="AE"></option>
+          <option value="EE"></option>
+          <option value="ME"></option>
+          <option value="PHYS"></option>
+          <option value="CIE"></option>
+          <option value="EEPH"></option>
+          <option value="CHE"></option>
+          <option value="MSE"></option>
+          <option value="CHEM"></option>
+          <option value="BIOE"></option>
+          <option value="CE"></option>
+          <option value="ARE"></option>
+          <option value="ITD"></option>
+          <option value="SSC"></option>
+          <option value="ESE"></option>
+          <option value="CS"></option>
+          <option value="SWE"></option>
+          <option value="COE"></option>
+          <option value="ISE"></option>
+          <option value="MATH"></option>
+          <option value="AS"></option>
+          <option value="PETE"></option>
+          <option value="GEOP"></option>
+          <option value="GEOL"></option>
+          <option value="MINE"></option>
+          <option value="HRM"></option>
+          <option value="FIN"></option>
+          <option value="MKT"></option>
+          <option value="ACCT"></option>
+          <option value="MIS"></option>
+</datalist>
+<input type="text" list="SF_major" name="major" autoComplete="on" required />
+
+
+
+
+
+
+
+
         </div>
 
-        <div className='SF_input-box'>
-          <label id='SF_year'>Year*</label>
-          <select name="year" required>
-  <option value="1">1</option>
-  <option value="2">2</option>
-  <option value="3">3</option>
-  <option value="4">4</option>
-  <option value="5">5</option>
-  <option value="6">5+</option>
-</select>
+        <div className='SF_input-list'>
+          <label id='SF_year_text'>Year*</label>
+
+
+          <datalist id="SF_year" name="year" required>
+
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="5+">5+</option>
+
+          </datalist>
+    <input type="text"  list="SF_year" name="year" autoComplete="on" required />
+
         </div>
+
+
+      </div>
+      
+
+
+
+      
 
         <div className='SF_input-box'>
           <label id='SF_password'>Password*</label>
