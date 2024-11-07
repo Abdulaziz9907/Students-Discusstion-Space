cfe8eb79cce1087a31b4767da3936616f3035469
Abdulaziz9907 <168546745+Abdulaziz9907@users.noreply.github.com>
2024-11-07 17:47:41 +0300

    main webpage complete

--- my-app/src/pages/desktop ui/main_webpage/main_webpage.js
+++ my-app/src/pages/desktop ui/main_webpage/main_webpage.js
@@ -4,6 +4,7 @@ import mw_logo3 from './elements/Vector3.png';
 import mw_logo4 from './elements/Vector4.png';
 import mw_logo5 from './elements/Vector5.png';
 import mw_logo6 from './elements/Vector6.png';
+import mw_search from './elements/search.png';
 
 function main_webpage() {
   return (
@@ -23,7 +24,10 @@ function main_webpage() {
      </div>
 
 <div class="mw_search-container">
+      <img src={mw_search} alt="search" id='mw_search-icon'/>
       <input type="text" placeholder="Search" class="mw_search-bar"></input>
+     
+
       </div>
 
       <img src={mw_logo3} alt="comp1" id='mw_Vec3' className='mw_LogImage'/>
@@ -32,8 +36,41 @@ function main_webpage() {
       <img src={mw_logo6} alt="comp1" id='mw_Vec6' className='mw_LogImage'/>
       
 
-      <div id='mw_suggestions'>
-      <p id='mw_text'>Most Visited Courses</p>
+      <div id='mw_suggestions_container'>
+
+        <div id='mw_suggestions'>
+      <p id='mw_text2'>Most Visited Courses</p>
+        
+
+        <div id='mw_sgst1'> 
+        <span class="mw_sgst1-text">Math208</span>
+        <button class="mw_sgst-button">View details</button>
+        
+
+        </div>
+
+        <div id='mw_sgst2'> 
+
+        <span class="mw_sgst2-text">COE301</span>
+        <button class="mw_sgst-button">View details</button>
+
+        </div>
+
+        <div id='mw_sgst3'> 
+
+        <span class="mw_sgst3-text">ENGL214</span>
+        <button class="mw_sgst-button">View details</button>
+
+        </div>
+
+        <div id='mw_sgst4'> 
+
+        <span class="mw_sgst4-text">SWE363</span>
+        <button class="mw_sgst-button">View details</button>
+
+        </div>
+
+        </div>
 
      </div>
       
