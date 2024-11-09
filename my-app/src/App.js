706912d01f3aff4a8c994af4506e24ab30af005c
MOHAMMED ALTHOBAITI <s202037740@kfupm.edu.sa>
2024-11-09 11:37:53 +0300

    Add rating page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,7 @@ import Signup from './pages/desktop ui/signup/signup';
 import Main_Webpage from './pages/desktop ui/main_webpage/main_webpage';
 import Account from './pages/desktop ui/account/account';
 import CourseRating from './pages/desktop ui/courseRating/courseRating';
+import AddRating from './pages/desktop ui/addRating/addRating';
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -18,6 +19,7 @@ function App() {
             <Route path="/Main Webpage" element={<Main_Webpage />} />
             <Route path="/Account" element={<Account />} />
             <Route path="/CourseRating" element={<CourseRating />} />
+            <Route path="/AddRating" element={<AddRating />} />
          </Routes>
       </BrowserRouter>         
   );
