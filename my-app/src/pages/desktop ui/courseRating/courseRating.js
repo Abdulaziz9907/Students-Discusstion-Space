3b6682383cc1924763203a1f1e572149e16470cc
MOHAMMED ALTHOBAITI <s202037740@kfupm.edu.sa>
2024-11-09 23:20:48 +0300

    complete cource rating page

--- my-app/src/pages/desktop ui/courseRating/courseRating.js
+++ my-app/src/pages/desktop ui/courseRating/courseRating.js
@@ -1,47 +1,35 @@
-import './courseRating.css';
+import React, { useState } from 'react';
 import Navbar from '../../../components/assests/Navbar/Navbar';
-
-import login_logo2 from './elements/Vector2.png';
-import login_logo3 from './elements/Vector3.png';
-import login_logo4 from './elements/Vector4.png';
-import login_logo5 from './elements/Vector5.png';
-import login_logo6 from './elements/Vector6.png';
-import login_logo from './elements/Component 1.png';
-
+import './courseRating.css';
 
 
 function CourseRating() {
+  const [rating, setRating] = useState(0);
+
+  const handleRating = (newRating) => {
+    setRating(newRating);
+  };
+
   return (
-    <div className='parent'>
-
-
-
-      <img src={login_logo2} alt="comp1" id='loginVec2' className='loginLogImage'/>
-      <img src={login_logo3} alt="comp1" id='loginVec3' className='loginLogImage'/>
-      <img src={login_logo4} alt="comp1" id='loginVec4' className='loginLogImage'/>
-      <img src={login_logo5} alt="comp1" id='loginVec5' className='loginLogImage'/>
-      <img src={login_logo6} alt="comp1" id='loginVec6' className='loginLogImage'/>
-      
-      
-      <h1 className='course-name'>MATH208</h1>
-      
-
-      <div className="course-header">
+    <div>
+      <Navbar />
+      <div className='parent'>
         
-        <p >Course dificulty rating: <span className="rating-stars">★★★★☆</span></p>
-        <button>Add Rating</button>
-        
+        <h1 className='course-name'>MATH208</h1>
+
+        <div className="course-header">
+          <p>Course difficulty rating:</p>
+          <StarRating rating={rating} onClick={handleRating} />
+          <button className='add-rating-button'>Add Rating</button>
+        </div>
+        {/* Render reviews and pagination components here */}
+        <Review author="Abdulaziz" timestamp="23 hours ago" content="One of the easiest math courses"/>
+        <Review author="Rakan" timestamp="9 days ago" content="Needs a lot of practice"/>
+        <Review author="Abdulah" timestamp="2 years ago" content="The course will introduce students to the basics of linear algebra in \( \mathbb{R}^n \). and the basic types of differential equations and some of the various techniques for solving them. Nothing too difficult."/>
       </div>
-      {/* Render reviews and pagination components here */}
-      <Review author= "Mohammed" timestamp="12:00" content="Good"/>
-      <Review author= "Ahmed" timestamp="11:00" content="Not bad course "/>
-      <Review author= "Saud" timestamp="10:00" content="Easy course"/>
-
-
     </div>
   );
 }
-
 
 const Review = ({ author, timestamp, content }) => {
   return (
@@ -55,16 +43,24 @@ const Review = ({ author, timestamp, content }) => {
     </div>
   );
 };
+
+const StarRating = ({ rating, onClick }) => {
+  return (
+    <div className="rating-stars">
+      {[...Array(5)].map((star, index) => {
+        index += 1;
+        return (
+          <span
+            key={index}
+            className={index <= rating ? "on" : "off"}
+            onClick={() => onClick(index)}
+          >
+            &#9733;
+          </span>
+        );
+      })}
+    </div>
+  );
+};
+
 export default CourseRating;
-
-
-
-
-
-
-
-
-
-
-
-
