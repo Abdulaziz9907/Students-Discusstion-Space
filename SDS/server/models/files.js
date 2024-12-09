4bc56271df80c365b35f4354d6a9bfa48281ef06
lDEDA <52706477+lDEDA@users.noreply.github.com>
2024-12-09 06:44:57 +0300

    new file schema

--- SDS/server/models/files.js
+++ SDS/server/models/files.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
-const fileSchema = new mongoose.Schema({
+const FileSchema = new mongoose.Schema({
   courseId: { type: String, required: true },
   fileName: { type: String, required: true },
   filePath: { type: String, required: true },
+  fileUrl: { type: String, required: true },
   uploadedAt: { type: Date, default: Date.now },
 });
 
-const FileModel = mongoose.model('File', fileSchema);
-
-module.exports = FileModel;
+module.exports = mongoose.model('File', FileSchema);
